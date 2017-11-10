6a18c67d61bfd14e3585c49f6a4fb7f5764f8789
AlexeuTsukanov <alexeuel@gmail.com>
2017-11-10 18:52:08 +0200

    Исправил задачу про ООП.

--- src/com/company/lesson14/task3/ReplaceTest.java
+++ src/com/company/lesson14/task3/ReplaceTest.java
@@ -2,25 +2,15 @@ package com.company.lesson14.task3;
 
 public class ReplaceTest {
     public static void main(String[] args) {
-        replaceString("Object oriented programming");
-        replaceOOP("palsd[pasld a[sdlasd object oriented programming asl;dm;alsd asdas");
+        Replace.replaceOOP("palsd[pasld a[sdlasd object oriented programming asl;dm;alsd asdas");
     }
+    public static class Replace {
+        final static String OOP = "object oriented programming";
 
-    public static void replaceString(String s) {
-        StringBuilder sb = new StringBuilder(20);
-        sb.append(s);
-        for (int i = 1; i < 3; i++) {
-            sb.delete(i, sb.indexOf(" ") + 1);
+        public static void replaceOOP(String s) {
+            StringBuilder sb = new StringBuilder(s);
+            sb.replace(sb.indexOf(OOP), sb.indexOf(OOP) + OOP.length(), "OOP");
+            System.out.println(sb);
         }
-        sb.delete(3, sb.length());
-        String end = sb.toString().toUpperCase();
-        System.out.println(end);
-    }
-
-    public static void replaceOOP(String s){
-        StringBuilder sb = new StringBuilder(20);
-        sb.append(s);
-        sb.replace(sb.indexOf("object oriented programming"),sb.indexOf("object oriented programming")+"object oriented programming".length(),"OOP");
-        System.out.println(sb);
     }
 }
